1c6cc75c3c459357f4e14a0d0cab765b5c7cfa27
Jason Taylor <jason@codetaylor.com>
2016-11-06 17:12:26 -0800

    initial commit

---
+++ src/main/java/com/sudoplay/mc/kormetals/KorMetals.java
@@ -0,0 +1,67 @@
+package com.sudoplay.mc.kormetals;
+
+import com.sudoplay.mc.kor.spi.Kor;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import static net.minecraftforge.fml.common.Mod.EventHandler;
+import static net.minecraftforge.fml.common.Mod.Instance;
+
+@net.minecraftforge.fml.common.Mod(
+    modid = KorMetals.MOD_ID,
+    version = KorMetals.VERSION,
+    name = KorMetals.NAME
+)
+public class KorMetals extends
+    Kor {
+
+  public static final String MOD_ID = "kormetals";
+  public static final String VERSION = "@VERSION@";
+  public static final String NAME = "Kor Metals";
+  public static final double JSON_CONFIGS_VERSION = 1.0;
+
+  @SuppressWarnings("unused")
+  @Instance
+  public static KorMetals INSTANCE;
+
+  @Override
+  public double getJsonConfigsVersion() {
+    return KorMetals.JSON_CONFIGS_VERSION;
+  }
+
+  @Override
+  public String getModId() {
+    return KorMetals.MOD_ID;
+  }
+
+  @Override
+  public String getModVersion() {
+    return KorMetals.VERSION;
+  }
+
+  @Override
+  public String getModName() {
+    return KorMetals.NAME;
+  }
+
+  @EventHandler
+  protected void onPreInitialization(FMLPreInitializationEvent event) {
+
+    this.registerModules(
+        // Register modules here
+    );
+
+    super.onPreInitialization(event);
+  }
+
+  @EventHandler
+  protected void onInitialization(FMLInitializationEvent event) {
+    super.onInitialization(event);
+  }
+
+  @EventHandler
+  protected void onPostInitialization(FMLPostInitializationEvent event) {
+    super.onPostInitialization(event);
+  }
+}
